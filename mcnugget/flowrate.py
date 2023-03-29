d32e451d1545dbd97026cc19a393e406f5a72c58
iausin <iausin@umich.edu>
2023-03-29 18:59:39 -0400

    editing flowmeter script

--- mcnugget/flowrate.py
+++ mcnugget/flowrate.py
@@ -3,7 +3,7 @@ from mcnugget.query import read
 from mcnugget.time import elapsed_seconds
 import numpy as np
 import matplotlib.pyplot as plt
-TR = synnax.TimeRange(1677367953382773500, 1677367965922904600)
+TR = synnax.TimeRange(1677367954335752200, 1677367959746683000)
 
 
 
@@ -12,9 +12,19 @@ DATA = read(TR,"ec.pressure[7]", "Time")
 
 # Set Variables for Pressure (also convert it to Pa) and Time
 Time = elapsed_seconds(DATA["Time"])
-DPs = DATA["ec.pressure[7]"] * 6895
+DP = DATA["ec.pressure[7]"] * 6895
 
+C = 0.995      #discharge coefficient
+inlet_diam = 0.92     # in. (measured ID of the inlet)
+throat_diam = 0.79     # in. (both copied from flowmeter mastersheet
+rho = 8.34             # lb/ft^3
+beta = throat_diam/inlet_diam  # diameter ratio (<1)
 
+m_dots = ((C*np.pi*(throat_diam**2)*((2*DP*rho)**0.5))/(4*((1-(beta**4))**0.5)))/2.20462
+
+plt.plot(Time, m_dots)
+print(m_dots)
+plt.show()
 
 
 
