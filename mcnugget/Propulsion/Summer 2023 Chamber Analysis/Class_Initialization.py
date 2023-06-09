dae79c7c6e340f78ac45f3ad8a41e36a3dc404bd
Logan Harrold <logharr@umich.edu>
2023-06-09 17:07:31 -0400

    Started initialzing classes with regen channel

---
+++ mcnugget/Propulsion/Summer 2023 Chamber Analysis/Class_Initialization.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+from pyfluids import Fluid, FluidsList, Input
+
+init = 1
+
+class Regen_Channel:     
+    L = init
+    di = init
+    do = init
+    A = init
+    dh = init
+    k = init
+    f = init
+
