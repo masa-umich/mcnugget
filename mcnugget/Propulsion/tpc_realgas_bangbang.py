0e98d98fa837912ba9432e305c731d1ab83e0cf5
Trevor-Jarvis-Umich <tjarvis@umich.edu>
2023-06-19 17:19:42 -0400

    Fluid variables and tank pressure set

--- mcnugget/Propulsion/tpc_realgas_bangbang.py
+++ mcnugget/Propulsion/tpc_realgas_bangbang.py
@@ -1,6 +1,35 @@
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import CoolProp.CoolProp as CP
 from pyfluids import Fluid, FluidsList
+from numpy import genfromtxt
 
-print("Hello World")
+# Copy of the fuel class from Logan's chamber analysis scripts (thanks btw)
+
+Conductivity = genfromtxt('Conductivity.csv', delimiter=',')
+Heat_Capacity = genfromtxt('Heat_Capacity.csv', delimiter=',')
+
+# Interopolates Fuel Properties at a given temperature
+# Inputs: Temperature (K)
+# Outputs: Conductivity, Viscosity, Prandtl Number, Specific Heat, Density
+class Fuel:
+    def __init__(self, T):
+        self.T = T
+
+    @property
+    def T(self):
+        return self._T
+
+    @T.setter
+    def T(self, T):
+        self._T = T
+        self.k = np.interp(T, Conductivity[:, 0], Conductivity[:, 1])
+        self.u = np.exp(2.5585 + (-3.505 / (T / 273.15)) - (3.412 * np.log(T / 273.15)) + (2.1551 * (T / 273.15)**(-3.145))) * 0.0008
+        self.cp = np.interp(T, Heat_Capacity[:, 0], Heat_Capacity[:, 1])
+        self.rho = 287.67 * 0.53365**(-(1 + (1 - (T / 574.262))**0.6289))
+
+# Initialize fluid variables
+RP1 = Fuel(273)
+LOx = Fluid(FluidsList.Oxygen)
+# Target tank pressure
+P_tank = 500*6894.76
