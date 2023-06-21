dd01a1d72b8e932e3410e0e78fab3107ba5ac230
Trevor-Jarvis-Umich <tjarvis@umich.edu>
2023-06-21 17:55:18 -0400

    graph made for isothermal case, numbers are off though

--- mcnugget/Propulsion/BangBangTPC/tpc_realgas_bangbang.py
+++ mcnugget/Propulsion/BangBangTPC/tpc_realgas_bangbang.py
@@ -35,20 +35,43 @@ N2 = Fluid(FluidsList.Nitrogen).with_state(Input.pressure(4500*6894.76),Input.te
 
 # Target tank pressure
 P_tank = 500*6894.76
+Tank_psi = 500
 
-# Estimated Cd
+# Estimated Cd and collapse factor
 Cd = 0.61
+Cf = 2.1
 
-# mdots out of the tanks
+# mdots at T0 
 mdot_F = 1.9967
 mdot_L = 3.9934
 
 # Temperature for isentropic calcs
 T0 = 290
 
-# Vdots at T0 (may go unused)
-vdot_F = mdot_F/RP1.rho 
+# vdots at T0
+vdot_F = mdot_F/RP1.rho
 vdot_L = mdot_L/LOx.density
 
-print(vdot_F)
-print(vdot_L)
+# gamma (seems like it should be constant for the scope of the problem)
+gamma = 1.4
+
+# COPV pressure array
+COPV = np.linspace(P_tank/(2/(gamma+1))**(gamma/(gamma-1)),4500*6894.76,1000)
+COPV_psi = np.linspace(Tank_psi/(2/(gamma+1))**(gamma/(gamma-1)),4500,1000)
+
+# isothermal curves, outputs in square inches
+Isotherm_F = np.zeros(1000)
+Isotherm_L = np.zeros(1000)
+
+for x in range(1000):
+    N2 = N2.with_state(Input.pressure(COPV[x]),Input.temperature(16.85))
+    Isotherm_F[x] = mdot_F/(Cd*(gamma*N2.density*COPV[x]*(2/(gamma+1))**((gamma+1)/(gamma-1)))**(1/2))*1550
+    Isotherm_L[x] = mdot_L/(Cd*(gamma*N2.density*COPV[x]*(2/(gamma+1))**((gamma+1)/(gamma-1)))**(1/2))*1550*Cf
+
+# plot the ideal area isothermal curves
+line_1 = plt.plot(COPV_psi,Isotherm_F,'-r',label='RP1')
+line_2 = plt.plot(COPV_psi,Isotherm_L,'-b',label='LOx')
+plt.legend()
+plt.xlabel("COPV Pressure (psi)")
+plt.ylabel("Area (in^2)")
+plt.show()
