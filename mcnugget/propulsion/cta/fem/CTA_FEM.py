39d007166e27a1f702d3558f9c68ce95f5b856cd
= <=>
2024-01-11 12:27:26 -0500

    adding folder for chamber thermal analysis

---
+++ mcnugget/propulsion/cta/fem/CTA_FEM.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 10 19:30:13 2024
+
+@author: natecamp
+"""
+
+def CTA_FEM:
+    '''A class representing a finite element thermal model of a thrust chamber.
+    
+    '''
+    
+    
+    
+
---
+++ mcnugget/propulsion/cta/fem/run_cta_fem.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 10 19:27:21 2024
+
+@author: natecamp
+"""
+
+import CTA_FEM
+
+
