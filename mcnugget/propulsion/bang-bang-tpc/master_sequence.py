ff16fec3dcf8ada6b089af3cbe65834a26162373
Evan Hekman <ehekman@umich.edu>
2024-02-02 19:50:09 -0500

    autosequence_valve class

--- mcnugget/propulsion/bang-bang-tpc/master_sequence.py
+++ mcnugget/propulsion/bang-bang-tpc/master_sequence.py
@@ -9,7 +9,9 @@ client = sy.Synnax(
     secure=False
 )
 
-class autosequencevalve:
+INC_DELAY = 1
+
+class autosequence_valve:
     # defines a valve with the following properties:
     
     # chan: the human-readable name for the channel
@@ -22,17 +24,19 @@ class autosequencevalve:
     # inc_wait: the delay between pressure incrementation (in seconds)
 
     # this function is called when a new valve is initiated
-    def __init__(self, chan: str, cmd: str, ack: str, press: str, target: int, max: int, inc: int, inc_wait: float):
+    def __init__(self, chan: str, cmd: str, ack: str, press: str, final_target: int, max: int, inc: int, inc_delay=INC_DELAY):
         self.chan = chan
         self.cmd = cmd
         self.ack = ack
         self.press = press
-        self.target = target
+        self.target = final_target
         self.max = max
         self.inc = inc
+        self.inc_delay = inc_delay
     
     # this function runs until the pressure is at the target pressure
     def run(self):
+        # this gives the function access to the channels
         with client.control.acquire(
             "bang_bang_tpc",
             write=[
@@ -41,26 +45,61 @@ class autosequencevalve:
             read=self.ack,
             write_authorities=[255]
         ) as auto:
+            # this increments the pressure until it reaches the target
+            print(f"Opening {self.chan}")
+            partial_target = self.inc
             while True:
-                print(f"Opening {self.chan}")
+                print(f"pressurizing to {partial_target}")
+
+                # opens valve
                 auto[self.cmd] = 1
-                auto.wait_until(lambda c: self.run_valve(self, auto))
+
+                # waits until the valve reaches the target pressure
+                auto.wait_until(lambda c: self.run_valve(c, partial_target))
+                
+                # closes valve
                 auto[self.cmd] = 0
+
+                # checks if the valve is fully pressurized, otherwise increments the target
                 if auto[self.press] >= self.target:
                     print(f"final target reached for valve {self.chan}")
                     break
-                time.sleep()
+                partial_target += self.inc
 
-    def run_valve(self, auto):
+                # slight delay for safety reasons
+                time.sleep(self.inc_delay)
+
+    # this function checks the pressure against target pressure and max pressure
+    # it returns true iff the pressure has reached the target
+    def run_valve(self, auto, target: int):
         pressure = auto[self.press]
         open = auto[self.ack]
 
+        # shuts off if the pressure is above the max
         if pressure > self.max:
-            print(f"closing valve {self.chan}")
-            auto[self.cmd] = False
+            if not open:
+                print(f"closing valve {self.chan}")
+            auto[self.cmd] = False  # this executes either way for safety
 
-        elif pressure > self.target:
-            print(f"reached target for valve {self.chan}")
+        # returns True if the target pressure is reached
+        elif pressure >= self.target:
+            if open:
+                print(f"reached target for valve {self.chan}")
+                return True
+        
+        # if the pressure has not reached the target, opens the valve
+        elif pressure < self.target:
+            if not open:
+                print(f"opening valve {self.chan}")
+                auto[self.cmd] = True
+
+    # closes the valve
+    def shut_down(self, auto):
+        print(f"finished - closing {self.cmd}")
+        auto[self.cmd] = False
 
 
-        return pressure < 15
+# closes all the valves in the list
+def shut_down(self, valves):
+    for valve in valves:
+        valve.shut_down()
