8261b7a5292ee152c7302a1e2a1b05098c5f71a9
Evan Hekman <ehekman@umich.edu>
2024-02-09 12:44:52 -0500

    fixed auto handling, open and close methods for valve

--- mcnugget/propulsion/bang-bang-tpc/auto_utilities.py
+++ mcnugget/propulsion/bang-bang-tpc/auto_utilities.py
@@ -1,60 +1,48 @@
 from synnax import Synnax, Controller
+import time
 
 # this defines a class that can be used for both regular valves and vents
 class Valve:
-    def __init__(self, auto: Controller, name: str, cmd: str, ack: str, default_open: bool, 
-                mawp: float, requires_confirm: bool = True):
+    def __init__(self, auto: Controller, name: str, cmd: str, ack: str, normally_open: bool, 
+                mawp: float, wait_for_ack: bool = True):
         self.name = name
-        self.cmd = cmd
-        self.ack = ack
-        self.default_open = default_open
+        self.cmd_chan = cmd
+        self.ack_chan = ack
+        self.normally_open = normally_open
         self.auto = auto
-        self.requires_confirm = requires_confirm
+        self.wait_for_ack = wait_for_ack
         self.mawp = mawp
 
-    # this energizes the valve until the target pressure is reached
-    def pressurize(self, pressure: str, target: float, inc: float):
-        partial_target = inc
-        if self.requires_confirm:
-            input(f"pressurizing {self.name} to {target} - press Enter to continue")
-        while True:
-            print(f"pressurizing {self.name} to {partial_target}")
-            self.auto[self.cmd] = True
-            self.auto.wait_until(lambda auto: auto[pressure] >= partial_target)
-            if partial_target >= target:
-                print(f"{self.name} has reached {target}")
-                break
-            partial_target += inc
+    # energizes valve, de-energizes vent
+    def open(self):
+        # for reg. valve, normally_open is false so cmd is set to True
+        # for vent valve, normally_open is true so cmd is set to False
+        self.auto[self.cmd_chan] = not self.normally_open
+        self.auto.wait_until(self.ack_chan != self.normally_open)
 
-    # closes a vent or opens a valve
-    def energize(self):
-        self.auto[self.cmd] = not self.default_open
-
-    # opens a vent or closes a valve
-    def de_energize(self):
-        self.auto[self.cmd] = self.default_open
+    # de-energizes valve, energizes vent
+    def close(self):
+        # for reg. valve, normally_open is false so cmd is set to False
+        # for vent valve, normally_open is true so cmd is set to True
+        self.auto[self.cmd_chan] = self.normally_open
+        self.auto.wait_until(self.ack_chan == self.normally_open)
 
     # returns true iff the valve is below the MAWP
     def check_safe(self, pressure: str):
         return self.auto[pressure] < self.mawp
 
-
-# this function initializes `auto` for the channels specified
-def initialize_for_autosequence(cmds: [str], acks: [str], pressures: [str]):
-    client = Synnax(
-        host="localhost",
-        port=9090,
-        username="synnax",
-        password="seldon",
-        secure=False
-    )
-    return client.control.acquire(
-        "bang_bang_tpc",
-        write=[
-            cmd for cmd in cmds
-        ],
-        read=[
-            ack_or_press for ack_or_press in acks + pressures
-        ],
-        write_authorities=[255]
-    )
+# this energizes the valve until the target pressure is reached
+def pressurize(self, valve: Valve, pressure: str, target: float, inc: float, delay: float = 1):
+    partial_target = inc
+    if self.requires_confirm:
+        input(f"pressurizing {self.name} to {target} - press Enter to continue")
+    while True:
+        print(f"pressurizing {self.name} to {partial_target}")
+        valve.open()
+        self.auto.wait_until(lambda auto: auto[pressure] >= partial_target)
+        valve.close()
+        time.sleep(delay)
+        if partial_target >= target:
+            print(f"{self.name} has reached {target}")
+            break
+        partial_target += inc
--- mcnugget/propulsion/bang-bang-tpc/feb_10_autosequence.py
+++ mcnugget/propulsion/bang-bang-tpc/feb_10_autosequence.py
@@ -48,148 +48,147 @@ BOUND_2 = 402.5
 MAXIMUM = TARGET_1 + 20
 MINIMUM = BOUND_2 - 20
 
+# this function initializes `auto` for the channels specified
+with client.control.acquire(
+        "bang_bang_tpc",
+        write=[
+            TESCOM_1_CMD,
+            TESCOM_2_CMD,
+            SCUBA_CMD,
+            VENT_CMD,
+            MPV_CMD,
+        ],
+        read=[
+            TESCOM_1_ACK,
+            TESCOM_2_ACK,
+            SCUBA_PT,
+            L_STAND_PT,
+        ],
+        write_authorities=[255]
+    ) as auto:
 
-# this initializes a connection to the client with access to all the needed channels
-auto = auto_utilities.initialize_for_autosequence(
-    [
-        TESCOM_1_CMD,
-        TESCOM_2_CMD,
-        SCUBA_CMD,
-        VENT_CMD,
-        MPV_CMD,
-    ],
-    [
-        TESCOM_1_ACK,
-        TESCOM_2_ACK,
-    ],
-    [
-        SCUBA_PT,
-        L_STAND_PT,
-    ]
-)
-
-TESCOM_1 = auto_utilities.Valve(auto=auto, name="tescom 1", cmd=TESCOM_1_CMD, ack=TESCOM_1_ACK, 
-                                default_open=False, mawp=MAXIMUM, requires_confirm=False)
-TESCOM_2 = auto_utilities.Valve(auto=auto, name="tescom 2", cmd=TESCOM_2_CMD, ack=TESCOM_2_ACK, 
-                                default_open=False, mawp=MAXIMUM, requires_confirm=False)
-ISO_PRESS = auto_utilities.Valve(auto=auto, name="ISO Press", cmd=SCUBA_CMD, ack=None, 
-                                default_open=False, mawp=MAXIMUM, requires_confirm=False)
-MPV = auto_utilities.Valve(auto=auto, name="MPV", cmd=MPV_CMD, ack=None, 
-                                default_open=False, mawp=MAXIMUM, requires_confirm=False)
-VENT = auto_utilities.Valve(auto=auto, name="MPV Vent", cmd=VENT_CMD, ack=None, 
-                                default_open=True, mawp=MAXIMUM, requires_confirm=False)
+    TESCOM_1 = auto_utilities.Valve(auto=auto, name="tescom 1", cmd=TESCOM_1_CMD, ack=TESCOM_1_ACK, 
+                                    default_open=False, mawp=MAXIMUM, requires_confirm=False)
+    TESCOM_2 = auto_utilities.Valve(auto=auto, name="tescom 2", cmd=TESCOM_2_CMD, ack=TESCOM_2_ACK, 
+                                    default_open=False, mawp=MAXIMUM, requires_confirm=False)
+    ISO_PRESS = auto_utilities.Valve(auto=auto, name="ISO Press", cmd=SCUBA_CMD, ack=None, 
+                                    default_open=False, mawp=MAXIMUM, requires_confirm=False)
+    MPV = auto_utilities.Valve(auto=auto, name="MPV", cmd=MPV_CMD, ack=None, 
+                                    default_open=False, mawp=MAXIMUM, requires_confirm=False)
+    VENT = auto_utilities.Valve(auto=auto, name="MPV Vent", cmd=VENT_CMD, ack=None, 
+                                    default_open=True, mawp=MAXIMUM, requires_confirm=False)
 
 
-def run_tpc(auto: Controller):
-    pressure = auto[L_STAND_PT]
-    one_open = auto[TESCOM_1_ACK]
-    two_open = auto[TESCOM_2_ACK]
+    def run_tpc(auto: Controller):
+        pressure = auto[L_STAND_PT]
+        one_open = auto[TESCOM_1_ACK]
+        two_open = auto[TESCOM_2_ACK]
 
-    while True:
-        # aborts if the pressure is above the accepted maximum
-        if pressure > MAXIMUM:
-            print("pressure has exceeded acceptable range - ABORTING")
-            TESCOM_1.de_energize()
-            TESCOM_2.de_energize()
-            break
-
-        # if pressure is above TARGET_1, closes both valves
-        if pressure > TARGET_1:
-            if one_open or two_open:
-                print(f"pressure above {BOUND_1} - closing both valves")
+        while True:
+            # aborts if the pressure is above the accepted maximum
+            if pressure > MAXIMUM:
+                print("pressure has exceeded acceptable range - ABORTING")
                 TESCOM_1.de_energize()
                 TESCOM_2.de_energize()
+                break
 
-        # if the pressure is below BOUND_1, opens TESCOM_1
-        if pressure < BOUND_1:
-            if not one_open:
-                print(f"pressure below {BOUND_1} - opening valve 1")
-                TESCOM_1.energize()
-            
-        # if the pressure is above TARGET_2, closes TESCOM_2
-        if pressure > TARGET_2:
-            if two_open:
-                print(f"pressure above {TARGET_2} - closing valve 2")
-                TESCOM_2.de_energize()
+            # if pressure is above TARGET_1, closes both valves
+            if pressure > TARGET_1:
+                if one_open or two_open:
+                    print(f"pressure above {BOUND_1} - closing both valves")
+                    TESCOM_1.de_energize()
+                    TESCOM_2.de_energize()
 
-        # if the pressure is below BOUND_2, opens TESCOM_2
-        if pressure < BOUND_2:
-            if not two_open:
-                print(f"pressure below {BOUND_2} - opening valve 2")
-                TESCOM_2.energize()
+            # if the pressure is below BOUND_1, opens TESCOM_1
+            if pressure < BOUND_1:
+                if not one_open:
+                    print(f"pressure below {BOUND_1} - opening valve 1")
+                    TESCOM_1.energize()
+                
+            # if the pressure is above TARGET_2, closes TESCOM_2
+            if pressure > TARGET_2:
+                if two_open:
+                    print(f"pressure above {TARGET_2} - closing valve 2")
+                    TESCOM_2.de_energize()
 
-        # if the pressure is below MINIMUM, opens both valves
-        if pressure < MINIMUM:
-            if not one_open or not two_open:
-                print(f"pressure below minimum of {MINIMUM} - opening both valves")
-                TESCOM_1.energize()
-                TESCOM_2.energize()
+            # if the pressure is below BOUND_2, opens TESCOM_2
+            if pressure < BOUND_2:
+                if not two_open:
+                    print(f"pressure below {BOUND_2} - opening valve 2")
+                    TESCOM_2.energize()
 
-        # if the pressure drops below 15, the tanks are mostly empty and the test is finished
-        return pressure < 15
+            # if the pressure is below MINIMUM, opens both valves
+            if pressure < MINIMUM:
+                if not one_open or not two_open:
+                    print(f"pressure below minimum of {MINIMUM} - opening both valves")
+                    TESCOM_1.energize()
+                    TESCOM_2.energize()
 
-try:
-    print("Starting TPC Test. Setting initial system state.")
-    ISO_PRESS.de_energize()
-    TESCOM_1.de_energize()
-    TESCOM_2.de_energize()
-    MPV.de_energize()
-    VENT.de_energize()
+            # if the pressure drops below 15, the tanks are mostly empty and the test is finished
+            return pressure < 15
 
-    time.sleep(2)
+    try:
+        print("Starting TPC Test. Setting initial system state.")
+        ISO_PRESS.de_energize()
+        TESCOM_1.de_energize()
+        TESCOM_2.de_energize()
+        MPV.de_energize()
+        VENT.de_energize()
 
-    print(f"Pressing SCUBA and L-Stand to 80 PSI")
-    TESCOM_1.energize()
-    TESCOM_2.energize()
-    ISO_PRESS.energize()
-    # pressurizes SCUBA (and L-stand by extension) to 80 psi in 8 increments
-    ISO_PRESS.pressurize(SCUBA_PT, 80, 10)
-    
-    print("closing TESCOMS and pressing SCUBA to 425 psi")
-    TESCOM_1.de_energize()
-    TESCOM_2.de_energize()
-    # pressurizes SCUBA to 425 psi in 10 increments
-    ISO_PRESS.pressurize(SCUBA_PT, 425, 34.5)
-    
-    # closes ISO_PRESS in preparation for TPC control
-    ISO_PRESS.de_energize()
+        time.sleep(2)
 
-    print("SCUBA pressurized to 425 psi - beginning TPC control in 5")
-    time.sleep(1)
-    print("4")
-    time.sleep(1)
-    print("3")
-    time.sleep(1)
-    print("2")
-    time.sleep(1)
-    print("1")
-    time.sleep(1)
+        print(f"Pressing SCUBA and L-Stand to 80 PSI")
+        TESCOM_1.energize()
+        TESCOM_2.energize()
+        ISO_PRESS.energize()
+        # pressurizes SCUBA (and L-stand by extension) to 80 psi in 8 increments
+        ISO_PRESS.pressurize(SCUBA_PT, 80, 10)
+        
+        print("closing TESCOMS and pressing SCUBA to 425 psi")
+        TESCOM_1.de_energize()
+        TESCOM_2.de_energize()
+        # pressurizes SCUBA to 425 psi in 10 increments
+        ISO_PRESS.pressurize(SCUBA_PT, 425, 34.5)
+        
+        # closes ISO_PRESS in preparation for TPC control
+        ISO_PRESS.de_energize()
 
-    print("Opening MPV")
-    MPV.energize()
-    start = sy.TimeStamp.now()  
+        print("SCUBA pressurized to 425 psi - beginning TPC control in 5")
+        time.sleep(1)
+        print("4")
+        time.sleep(1)
+        print("3")
+        time.sleep(1)
+        print("2")
+        time.sleep(1)
+        print("1")
+        time.sleep(1)
 
-    auto.wait_until(run_tpc())
+        print("Opening MPV")
+        MPV.energize()
+        start = sy.TimeStamp.now()  
 
-    print("Test complete. Safeing System")
+        auto.wait_until(run_tpc())
 
-    rng = client.ranges.create(
-        name=f"{start.__str__()[11:16]} Bang Bang TPC Sim",
-        time_range=sy.TimeRange(start, sy.TimeStamp.now()),
-    )
+        print("Test complete. Safeing System")
 
-    ISO_PRESS.de_energize()
-    TESCOM_1.energize()
-    TESCOM_2.energize()
-    VENT.energize()
-    MPV.energize()
+        rng = client.ranges.create(
+            name=f"{start.__str__()[11:16]} Bang Bang TPC Sim",
+            time_range=sy.TimeRange(start, sy.TimeStamp.now()),
+        )
 
-except KeyboardInterrupt:
-    print("Test interrupted. Safeing System")
-    ISO_PRESS.de_energize()
-    TESCOM_1.energize()
-    TESCOM_2.energize()
-    VENT.energize()
-    MPV.energize()
+        ISO_PRESS.de_energize()
+        TESCOM_1.energize()
+        TESCOM_2.energize()
+        VENT.energize()
+        MPV.energize()
 
-print("end of autosequence - waiting 60 seconds")
+    except KeyboardInterrupt:
+        print("Test interrupted. Safeing System")
+        ISO_PRESS.de_energize()
+        TESCOM_1.energize()
+        TESCOM_2.energize()
+        VENT.energize()
+        MPV.energize()
+
+    print("end of autosequence - waiting 60 seconds")
