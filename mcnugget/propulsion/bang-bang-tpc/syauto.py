6f99cee900cd0c4fd45a32547b630d9168c29493
Evan Hekman <ehekman@umich.edu>
2024-02-09 13:40:04 -0500

    improved valve functions

--- mcnugget/propulsion/bang-bang-tpc/syauto.py
+++ mcnugget/propulsion/bang-bang-tpc/syauto.py
@@ -13,64 +13,50 @@ class Valve:
             ack: str = "",
             normally_open: bool = False,
             mawp: float = 0,
-            requires_confirm: bool = False
+            wait_for_ack: bool = False
     ):
         self.name = name
         self.cmd_chan = cmd
         self.ack = ack
         self.normally_open = normally_open
         self.auto = auto
-        self.requires_confirm = requires_confirm
+        self.wait_for_ack = wait_for_ack
         self.mawp = mawp
 
-    # this energizes the valve until the target pressure is reached
-    def pressurize(self, pressure: str, target: float, inc: float, delay: float = 0.5):
-        partial_target = inc
-        if self.requires_confirm:
-            input(f"pressurizing {self.name} to {target} - press Enter to continue")
-        while True:
-            print(f"pressurizing {self.name} to {partial_target}")
-            self.open()
-            self.auto.wait_until(lambda auto: auto[pressure] >= partial_target)
-            self.close()
-            time.sleep(delay)
-            if partial_target >= target:
-                print(f"{self.name} has reached {target}")
-                break
-            partial_target += inc
 
-    # closes a vent or opens a valve
     def open(self):
+        # for reg. valve, normally_open is false so cmd is set to True
+        # for vent valve, normally_open is true so cmd is set to False
         self.auto[self.cmd_chan] = not self.normally_open
+        self.auto.wait_until(self.ack_chan != self.normally_open)
 
-    # opens a vent or closes a valve
+
     def close(self):
+        # for reg. valve, normally_open is false so cmd is set to False
+        # for vent valve, normally_open is true so cmd is set to True
         self.auto[self.cmd_chan] = self.normally_open
+        self.auto.wait_until(self.ack_chan == self.normally_open)
 
     # returns true iff the valve is below the MAWP
     def check_safe(self, pressure: str):
         return self.auto[pressure] < self.mawp
 
 
-# this function initializes `auto` for the channels specified
-def initialize_for_autosequence(cmds: [str], acks: [str], pressures: [str]):
-    client = Synnax(
-        host="localhost",
-        port=9090,
-        username="synnax",
-        password="seldon",
-        secure=False
-    )
-    return client.control.acquire(
-        "bang_bang_tpc",
-        write=[
-            cmd for cmd in cmds
-        ],
-        read=[
-            ack_or_press for ack_or_press in acks + pressures
-        ],
-        write_authorities=[255]
-    )
+def open_close_many_valves(auto: Controller, valves_to_close: [Valve], valves_to_open: [Valve]):
+    dict1 = {valve.cmd_chan: not valve.normally_open for valve in valves_to_close}
+    dict2 = {valve.cmd_chan: valve.normally_open for valve in valves_to_open}
+    dict = dict1 + dict2
+    auto.set({
+        dict
+    })
+
+def open_close_many_valves(auto: Controller, valves_to_close: [Valve], valves_to_open: [Valve]):
+    auto.set({
+        valve.cmd_chan: (not valve.normally_open) if valve in valves_to_close 
+        else valve.normally_open 
+        for valve in valves_to_open
+    })
+    # trust
 
 
 def close_all(auto: Controller, valves: list[Valve]):
@@ -79,3 +65,18 @@ def close_all(auto: Controller, valves: list[Valve]):
 
 def open_all(auto: Controller, valves: list[Valve]):
     auto.set({valve.cmd_chan: not valve.normally_open for valve in valves})
+
+
+# this energizes the valve until the target pressure is reached
+def pressurize(valve: Valve, pressure: str, target: float, inc: float, delay: float = 0.5):
+    partial_target = inc
+    while True:
+        print(f"pressurizing {valve.name} to {partial_target}")
+        valve.open()
+        valve.auto.wait_until(lambda auto: auto[pressure] >= partial_target)
+        valve.close()
+        time.sleep(delay)
+        if partial_target >= target:
+            print(f"{valve.name} has reached {target}")
+            break
+        partial_target += inc
