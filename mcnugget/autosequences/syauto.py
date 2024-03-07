40553c9e190578d757784ece9802ba585a6b6687
Evan Hekman <ehekman@umich.edu>
2024-03-07 00:14:18 -0500

    syauto improvements i hope

--- mcnugget/autosequences/syauto.py
+++ mcnugget/autosequences/syauto.py
@@ -2,12 +2,14 @@ import time
 import sys
 import synnax as sy
 from synnax.control.controller import Controller
+from typing import Union
 
 import synnax.control.controller
 print(synnax.control.controller.__file__)
 
-# this defines a class that can be used for both regular valves and vents
+
 class Valve:
+    # this defines a class that can be used for both regular valves and vents
     def __init__(
             self,
             auto: Controller,
@@ -94,20 +96,24 @@ class DualTescomValve:
 #         else valve.normally_open
 #         for valve in valves_to_open
 #     })
-            
+
 def open_close_many_valves(auto: Controller, valves_to_open: list[Valve], valves_to_close: list[Valve]):
     commands = {}
-    # Close valves to close
-    for valve in valves_to_open:
-        commands[valve.cmd_chan] = int(True)
     # Open valves to open
+    for valve in valves_to_open:
+        if valve.normally_open:
+            commands[valve.cmd_chan] = 0
+        else:
+            commands[valve.cmd_chan] = 1
+    # Close valves to close
     for valve in valves_to_close:
         if valve.normally_open:
-            commands[valve.cmd_chan] = int(True)
+            commands[valve.cmd_chan] = 1
         else:
-            commands[valve.cmd_chan] = int(False)
-    
+            commands[valve.cmd_chan] = 0
+
     auto.set(commands)
+
 
 def close_all(auto: Controller, valves: list[Valve]):
     auto.set({valve.cmd_chan: valve.normally_open for valve in valves})
@@ -117,32 +123,39 @@ def open_all(auto: Controller, valves: list[Valve]):
     auto.set({valve.cmd_chan: not valve.normally_open for valve in valves})
 
 
-# this energizes the valve until the target pressure is reached
-def pressurize(valve, pressure: str, target: float, inc: float, delay: float = 1):
+def pressurize(valve_s: Union[list[Valve], Valve], pressure: str, target: float, inc: float, delay: float = 1, custom_auto: Controller = None):
+    # this energizes the valve until the target pressure is reached
+    # valve_s can be either a single valve or a list
+
+    # single valve
+    if isinstance(valve_s, Valve):
+        print(f"pressurizing {valve_s.name} to {partial_target}")
+        # converts single valve to list with one valve so they are processed the same :o
+        if not custom_auto:
+            custom_auto = valve_s.auto
+        valve_s = [valve_s]
+
+    # list of valves
+    else:
+        print(f"pressurizing these valves to {partial_target}")
+        for v in valve_s:
+            print(v.name)
+        # assigns custom_auto to auto from first valve by default
+        if not custom_auto:
+            custom_auto = valve_s[0].auto
+
     partial_target = inc
     while True:
-        if isinstance(valve, dict):
-            valve_name = valve['name']
-            valve_open = valve['open']
-            valve_close = valve['close']
-            valve_auto = valve['auto']
-        elif isinstance(valve, Valve):
-            valve_name = valve.name
-            valve_open = valve.open
-            valve_close = valve.close
-            valve_auto = valve.auto
-        else:
-            raise ValueError("valve parameter must be either a dictionary or a Valve object")
-        
-        print(f"pressurizing {valve_name} to {partial_target}")
-        valve_open()
-        valve_auto.wait_until(lambda auto: auto[pressure] >= partial_target)
-        valve_close()
+        open_all(valve_s)
+        custom_auto.wait_until(
+            lambda anakin_skywalker: anakin_skywalker[pressure] >= partial_target)
+        close_all(valve_s)
         time.sleep(delay)
         if partial_target >= target:
-            print(f"{valve_name} has reached {target}")
+            print(f"valve(s) have reached {target}")
             break
         partial_target += inc
+
 
 def purge(valves: list[Valve], duration: float = 1):
     prev_time = time.time()
