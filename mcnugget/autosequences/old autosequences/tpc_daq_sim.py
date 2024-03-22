4703cccfc09fcfa3df354ae9c9761cb2a0c86cd7
Evan Hekman <ehekman@umich.edu>
2024-03-22 10:19:20 -0400

    keep tpc_daq_sim as reference only

--- mcnugget/autosequences/old autosequences/tpc_daq_sim.py
+++ mcnugget/autosequences/old autosequences/tpc_daq_sim.py
@@ -95,8 +95,10 @@ MPV_LAST_OPEN = None
 scuba_pressure = 0
 l_stand_pressure = 0
 
-
-WRITE_TO = [
+with client.new_streamer([TPC_1_OPEN_CMD, TPC_2_OPEN_CMD, TPC_1_CLOSE_CMD, TPC_2_CLOSE_CMD, MPV_CMD, PRESS_ISO_CMD, VENT_CMD, ]) as streamer:
+    with client.new_writer(
+            sy.TimeStamp.now(),
+            channels=[
                 DAQ_TIME,
                 TPC_1_OPEN_ACK,
                 TPC_2_OPEN_ACK,
@@ -108,11 +110,6 @@ WRITE_TO = [
                 L_STAND_PT,
                 SCUBA_PT,
             ]
-READ_FROM = [TPC_1_OPEN_CMD, TPC_2_OPEN_CMD, TPC_1_CLOSE_CMD, TPC_2_CLOSE_CMD, MPV_CMD, PRESS_ISO_CMD, VENT_CMD, ]
-with client.new_streamer(READ_FROM) as streamer:
-    with client.new_writer(
-            sy.TimeStamp.now(),
-            channels=
     ) as w:
         i = 0
         while True:
