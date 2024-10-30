3e8082c472f433cce094452d8c5c89a1d22e93df
evanhekman <ehekman@umich.edu>
2024-10-30 19:07:48 -0400

    press_fill and hotfire run

--- autosequences/press_fill.py
+++ autosequences/press_fill.py
@@ -44,7 +44,7 @@ After initializing the client and reference variables, the autosequence will:
 import time
 import synnax as sy
 from synnax.control.controller import Controller
-from mcnugget.autosequences import syauto
+from autosequences import syauto
 import statistics
 from collections import deque
 
