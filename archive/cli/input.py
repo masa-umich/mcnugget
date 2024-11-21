6f21ed6378af1ba55a9ea500eddb51ebbe2e44af
angad <rsangala@umich.edu>
2024-11-21 01:02:05 -0500

    New instrumentation script - input implemented

---
+++ archive/cli/input.py
@@ -0,0 +1,9 @@
+import click
+import math
+import pandas as pd
+from dataclasses import dataclass
+import gspread
+# from rich import print
+import synnax as sy
+# from mcnugget.client import client
+from rich.prompt import Prompt, Confirm
