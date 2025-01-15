b8f87f4790bc0f672d038e4140ce45d5ca85c907
evanhekman <ehekman@umich.edu>
2025-01-14 20:56:18 -0500

    angad remote changes

--- daq/main2.py
+++ daq/main2.py
@@ -1,11 +1,9 @@
 import synnax as sy
 from synnax.hardware import ni
+import pandas as pd
 
 client = sy.Synnax(
-    host="masasynnax.ddns.net",
-    port=9090,
-    username="synnax",
-    password="seldon"
+    host="masasynnax.ddns.net", port=9090, username="synnax", password="seldon"
 )
 
 analog_card = client.hardware.devices.retrieve(name="PCI-6225")
@@ -25,7 +23,7 @@ analog_task = ni.AnalogReadTask(
     sample_rate=sy.Rate.HZ * 50,
     stream_rate=sy.Rate.HZ * 10,
     data_saving=True,
-    channels=[]
+    channels=[],
 )
 
 # create digital read task
@@ -35,7 +33,7 @@ digital_read_task = ni.DigitalReadTask(
     sample_rate=sy.Rate.HZ * 50,
     stream_rate=sy.Rate.HZ * 10,
     data_saving=True,
-    channels=[]
+    channels=[],
 )
 
 # Create digital write task
@@ -44,7 +42,7 @@ digital_write_task = ni.DigitalWriteTask(
     device=digital_card.key,
     state_rate=sy.Rate.HZ * 50,
     data_saving=True,
-    channels=[]
+    channels=[],
 )
 
 client.hardware.tasks.configure(analog_task)
@@ -52,3 +50,50 @@ client.hardware.tasks.configure(digital_read_task)
 client.hardware.tasks.configure(digital_write_task)
 
 
+def main():
+    NUMBER_OF_ITEMS = 4  # change to take in input and then put into the function
+    # input_excel(r"C:\Users\ruchi\mcnugget\archive\cli\testing.xlsx", NUMBER_OF_ITEMS) #change to take in input and then put into the function
+    input_excel("/Users/evanhekman/masa/testing.xlsx", 4)
+
+
+def input_excel(file_path: str, item_num: int):
+    try:
+        df = pd.read_excel(file_path)
+    except FileNotFoundError as e:
+        print("File not found:", e)
+        return
+    except ValueError as e:
+        print("Invalid Excel file or format:", e)
+        return
+    except Exception as e:
+        print("Check sheet read in:", e)
+        return
+
+    df_new = df.head(item_num)
+    print("Excel file succesfully read.")
+    process_excel(df_new)
+
+
+def process_excel(file: pd.DataFrame):
+    print(f"reading {len(file)} rows")
+    for _, row in file.iterrows():
+        ai_0 = client.channels.create(
+            name="ai_0",
+            # Pass in the index key here to associate the channel with the index channel.
+            index=gse_ai_time.key,
+            data_type=sy.DataType.FLOAT32,
+            retrieve_if_name_exists=True,
+        )
+        print(len(row), row)
+        try:
+            if row["Sensor Type"] == "VLV":
+                populate_digital(row)
+            elif row["Sensor Type"] in ["PT", "TC", "LC"]:
+                populate_analogue(row)
+            else:
+                print("Check Sensor Type in Sheet")
+        except KeyError as e:
+            print(f"Missing column in row: {e}")
+            return
+        except Exception as e:
+            print(f"Error populating tasks: {e}")
