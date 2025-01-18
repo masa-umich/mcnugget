a6dcb107684baab1e863e0c678342b6403579023
evanhekman <ehekman@umich.edu>
2025-01-18 03:24:59 -0500

    ambientization does indeed seem correct

--- daq/ambientize.py
+++ daq/ambientize.py
@@ -3,19 +3,30 @@ import json
 import time
 import os
 
-client = synnax.Synnax()
+client = synnax.Synnax(
+    host="masasynnax.ddns.net",
+    port=9090,
+    username="synnax",
+    password="seldon",
+)
 
 ambient = {}
-channels_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "config", "channels.json")
-ambient_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "config", "ambient.json")
-with open(channels_path, 'r') as f:
+channels_path = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), "config", "channels.json"
+)
+ambient_path = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), "config", "ambient.json"
+)
+with open(channels_path, "r") as f:
     channels = json.load(f)
     channels = [c for c in channels.keys() if "pt" in c]
-    with client.control.acquire(name="ambientization", read=channels, write=None) as auto:
+    with client.control.acquire(
+        name="ambientization", read=channels, write=None
+    ) as auto:
         time.sleep(1)
         for channel in channels:
             ambient[channel] = float(auto[channel])
             print(f"{channel}: {ambient[channel]}")
 
-with open(ambient_path, 'w') as f:
+with open(ambient_path, "w") as f:
     json.dump(ambient, f, indent=4)
