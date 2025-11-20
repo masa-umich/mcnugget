8fff61934da1b734ee0486c89cf2dc8148c06bdf
Jack Hammerberg <jackmh@umich.edu>
2025-11-19 19:18:04 -0500

    More helper functions and config changes

--- autosequences/launch/launch.py
+++ autosequences/launch/launch.py
@@ -19,18 +19,20 @@ spinner.start()
 
 # 3rd party modules
 import synnax as sy
+from synnax.control.controller import Controller
 import yaml
 
 # standard modules
 import argparse
-from enum import Enum
+from enum import Enum, auto
+import time
 
 
 class State(Enum):
-    START = 1
-    BOTTLE_1_EQ = 2
-    BOTTLE_2_EQ = 3
-    BOTTLE_3_EQ = 4
+    START = auto()
+    BOTTLE_1_EQ = auto()
+    BOTTLE_2_EQ = auto()
+    BOTTLE_3_EQ = auto()
 
 
 # helper function to raise pretty errors
@@ -112,6 +114,9 @@ def parse_args() -> list:
     return args
 
 
+# TODO: make this return read_channels, write_channels, 
+# and a custom class with predefined config options
+# so that intellisense works with it 
 def parse_config(file_path: str):
     try:
         # using with will automatically close the file when done
@@ -123,62 +128,161 @@ def parse_config(file_path: str):
     except IOError as e:
         error_and_exit(f"Error: Unable to read the file '{file_path}'", exception=e)
 
+# Get all channels from the config that we might want to write to (valves)
+def get_write_channels(config):
+    write_channels = []
+    controllers = ["ebox", "flight_computer", "bay_board_1", "bay_board_2", "bay_board_3"]
+    for controller in controllers:
+        for controller in controllers:
+            prefix = ""
+            if controller == controllers[0]:
+                prefix = "gse"
+            elif controller == controllers[1]:
+                prefix = "fc"
+            elif controller == controllers[2]:
+                prefix = "bb1"
+            elif controller == controllers[3]:
+                prefix = "bb2"
+            elif controller == controllers[4]:
+                prefix = "bb3"
+        for valve in config[controller]["valves"].keys():
+            valve_id = str(config[controller]["valves"][valve])
+            write_channels.append(f"{prefix}_vlv_{valve_id}")
+    return write_channels
 
-def state_machine(args, config, client: sy.Synnax) -> None:
+# Get all channels from the config we might want to read from
+def get_read_channels(config):
+    read_channels = []
+    controllers = ["ebox", "flight_computer", "bay_board_1", "bay_board_2", "bay_board_3"]
+    for controller in controllers:
+        prefix = ""
+        if controller == controllers[0]:
+            prefix = "gse"
+        elif controller == controllers[1]:
+            prefix = "fc"
+        elif controller == controllers[2]:
+            prefix = "bb1"
+        elif controller == controllers[3]:
+            prefix = "bb2"
+        elif controller == controllers[4]:
+            prefix = "bb3"
+        for pt in config[controller]["pts"].keys():
+            pt_id = str(config[controller]["pts"][pt])
+            read_channels.append(f"{prefix}_pt_{pt_id}")
+        for tc in config[controller]["tcs"].keys():
+            tc_id = str(config[controller]["tcs"][tc])
+            read_channels.append(f"{prefix}_tc_{tc_id}")
+        for valve in config[controller]["valves"].keys():
+            valve_id = str(config[controller]["valves"][valve])
+            read_channels.append(f"{prefix}_state_{valve_id}")
+    return read_channels
+
+# Get the Synnax channel name corresponding to alias of the channel 
+# Example: "press_iso_1" would return "gse_pt_3"
+# All name matching is case-insensitive!
+# Duplicate name aren't handled!
+def get_channel_name(config, channel_name: str) -> str:
+    controllers = ["ebox", "flight_computer", "bay_board_1", "bay_board_2", "bay_board_3"]
+    for controller in controllers:
+        prefix = ""
+        if controller == controllers[0]:
+            prefix = "gse"
+        elif controller == controllers[1]:
+            prefix = "fc"
+        elif controller == controllers[2]:
+            prefix = "bb1"
+        elif controller == controllers[3]:
+            prefix = "bb2"
+        elif controller == controllers[4]:
+            prefix = "bb3"
+        for pt in config[controller]["pts"].keys():
+            pt_id = str(config[controller]["pts"][pt])
+            if pt.lower() == channel_name.lower():
+                return prefix + "_pt_" + pt_id
+        for tc in config[controller]["tcs"].keys():
+            tc_id = str(config[controller]["tcs"][tc])
+            if tc.lower() == channel_name.lower():
+                return prefix + "_tc_" + tc_id
+        for valve in config[controller]["valves"].keys():
+            valve_id = str(config[controller]["valves"][valve])
+            if valve.lower() == channel_name.lower():
+                return prefix + "_vlv_" + valve_id
+
+def get_sy_channel(config, client: sy.Synnax, channel_name: str) -> sy.Channel:
+    return client.channels.retrieve(get_channel_name(config, channel_name))
+
+@yaspin(text=colored("Running Autosequence...", "light_blue"))
+def state_machine(ctrl: Controller, config, client: sy.Synnax) -> None:
     state = State.START
-    while True:
+    try:
+        while True:
+            match state:
+                case (
+                    State.START
+                ):  # Prompt for state to jump to (bottle 1 default, or skip to 2 or 3)
+                    pass
+                case State.BOTTLE_1_EQ:
+                    # Open Press Fill Iso
+                    # Open Press Iso 1 for press rate #1, then close
+                    #   [MANUAL FOR NOW] During press, close Press Iso 1 if COPV temp exceeds the upper bound
+                    # Wait for the remainder of the 1 minute
+                    # Repeat with press rate #1 for 4 iterations
+                    # Open Press Iso 1 for press rate #2
+                    #   [MANUAL FOR NOW] During press, close Press Iso 1 if COPV temp exceeds the upper bound
+                    # Wait for the remainder of the 1 minute
+                    # Repeat until the COPV pressure is within x psi (equalization threshold) of the 6k Bottle PT 1, close Press Iso 1 and continue to next section
+                    pass
+                case State.BOTTLE_2_EQ:
+                    # First check that the 6k Bottle PT 2 is greater than the COPV pressure
+                    # Open Press Fill Iso
+                    # Then, open Press Iso 2 for press rate #1, then close
+                    # [MANUAL FOR NOW] During press, close Press Iso 2 if COPV temp exceeds the upper bound
+                    # Wait for the remainder of the 1 minute
+                    # Repeat with press rate #1 for 4 iterations
+                    # Open Press Iso 2 for press rate #2
+                    # [MANUAL FOR NOW] During press, close Press Iso 2 if COPV temp exceeds the upper bound
+                    # Wait for the remainder of the 1 minute
+                    # Repeat until the COPV pressure is within x psi (equalization threshold) of 6k Bottle PT 2, close Press Iso 2 and continue to next section
+                    pass
+                case State.BOTTLE_3_EQ:
+                    # First check that the 6k Bottle PT 3 is greater than the COPV pressure
+                    # Open Press Fill Iso
+                    # Then, open Press Iso 3 for press rate #1, then close
+                    # [MANUAL FOR NOW] During press, close Press Iso 3 if COPV temp exceeds the upper bound
+                    # Wait for the remainder of the 1 minute
+                    # Repeat with press rate #1 for 4 iterations
+                    # Open Press Iso 3 for press rate #2
+                    # [MANUAL FOR NOW] During press, close Press Iso 3 if COPV temp exceeds the upper bound
+                    # Wait for the remainder of the 1 minute
+                    # Repeat until the COPV pressure is within x psi (equalization threshold) of 6k Bottle PT 3 OR at with the COPV target pressure bounds, then close Press Iso 3
+                    # If target pressure achieved and the 6k Bottle PT 3 is x psig greater than the COPV pressure, continuously monitor the COPV pressure, and cycle Press Iso 3 to TPC the COPV within the target pressure bounds
+                    pass
+                case (
+                    _
+                ):  # Default case, in our case we should never reach this and its invalid
+                    pass
+    except KeyboardInterrupt or Exception:
         match state:
-            case (
-                State.START
-            ):  # Prompt for state to jump to (bottle 1 default, or skip to 2 or 3)
+            case State.START:
+                # abort case for start state
                 pass
             case State.BOTTLE_1_EQ:
-                # Open Press Fill Iso
-                # Open Press Iso 1 for press rate #1, then close
-                # [MANUAL FOR NOW] During press, close Press Iso 1 if COPV temp exceeds the upper bound
-                # Wait for the remainder of the 1 minute
-                # Repeat with press rate #1 for 4 iterations
-                # Open Press Iso 1 for press rate #2
-                # [MANUAL FOR NOW] During press, close Press Iso 1 if COPV temp exceeds the upper bound
-                # Wait for the remainder of the 1 minute
-                # Repeat until the COPV pressure is within x psi (equalization threshold) of the 6k Bottle PT 1, close Press Iso 1 and continue to next section
+                # abort case for bottle_1_eq state
                 pass
-            case State.BOTTLE_2_EQ:
-                # First check that the 6k Bottle PT 2 is greater than the COPV pressure
-                # Open Press Fill Iso
-                # Then, open Press Iso 2 for press rate #1, then close
-                # [MANUAL FOR NOW] During press, close Press Iso 2 if COPV temp exceeds the upper bound
-                # Wait for the remainder of the 1 minute
-                # Repeat with press rate #1 for 4 iterations
-                # Open Press Iso 2 for press rate #2
-                # [MANUAL FOR NOW] During press, close Press Iso 2 if COPV temp exceeds the upper bound
-                # Wait for the remainder of the 1 minute
-                # Repeat until the COPV pressure is within x psi (equalization threshold) of 6k Bottle PT 2, close Press Iso 2 and continue to next section
-                pass
-            case State.BOTTLE_3_EQ:
-                # First check that the 6k Bottle PT 3 is greater than the COPV pressure
-                # Open Press Fill Iso
-                # Then, open Press Iso 3 for press rate #1, then close
-                # [MANUAL FOR NOW] During press, close Press Iso 3 if COPV temp exceeds the upper bound
-                # Wait for the remainder of the 1 minute
-                # Repeat with press rate #1 for 4 iterations
-                # Open Press Iso 3 for press rate #2
-                # [MANUAL FOR NOW] During press, close Press Iso 3 if COPV temp exceeds the upper bound
-                # Wait for the remainder of the 1 minute
-                # Repeat until the COPV pressure is within x psi (equalization threshold) of 6k Bottle PT 3 OR at with the COPV target pressure bounds, then close Press Iso 3
-                # If target pressure achieved and the 6k Bottle PT 3 is x psig greater than the COPV pressure, continuously monitor the COPV pressure, and cycle Press Iso 3 to TPC the COPV within the target pressure bounds
-                pass
-            case (
-                _
-            ):  # Default case, in our case we should never reach this and its invalid
-                pass
-
 
 def main() -> None:
     args = parse_args()
     config = parse_config(args.config)
     client = synnax_login(args)
-    state_machine(args, config, client)
+    print(colored("Initialization Complete!", "green"))
+    with client.control.acquire(
+        name="Launch Autosequence",
+        write_authorities=[200],
+        write=get_write_channels(config),
+        read=get_read_channels(config)
+    ) as ctrl:
+        state_machine(ctrl, config, client)
+    print(colored("Autosequence complete! have a nice day :)", "green"))
 
 
 if __name__ == "__main__":
